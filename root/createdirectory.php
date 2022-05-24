ac5f6ab28cb475433bd2d354da2622cee5cf1e7e
JOAN GAU GONELL <joangau18@hotmail.com>
2022-05-24 15:25:19 +0200

    create directory

---
+++ root/createdirectory.php
@@ -0,0 +1,9 @@
+<?php
+
+ $dir = $_POST['name'];
+
+ mkdir($dir,777);
+
+ header("location: index.php");
+
+ ?>
---
+++ root/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File manager</title>
+</head>
+<body>
+
+<h1>File manager</h1>
+
+<form action="createdirectory.php" method="POST">
+<label for="folderName">Enter the name of the folder</label>
+<input type="text" name="name" id="folderName">
+<input type="submit" value="Create">
+</form>
+</body>
+</html>
+
+<?php
+$path = ".";
+$dir = opendir($path) or die ("Unable to open directory");
+while($file = readdir($dir))
+{
+    if($file =="." || $file ==".." || $file =="index.php" || $file == "createdirectory.php" || $file == "deletedirectory.php")
+    continue;
+    echo"<a href='$file'$file<a href='deletedirectory.php?dir=$file'></a><br>";
+};
+
+closedir($dir);
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
